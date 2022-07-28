0553f0c5b723e364a547e778cb11c5c82a35b9ae
p1atdev <shu_teiei@outlook.jp>
2022-07-28 15:06:45 +0900

    Initial Commit

---
+++ Vivid/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Vivid
+//
+//  Created by p1atdev on 2022/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vivid/VividApp.swift
@@ -0,0 +1,17 @@
+//
+//  VividApp.swift
+//  Vivid
+//
+//  Created by p1atdev on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct VividApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
